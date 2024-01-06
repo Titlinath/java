8292bf6429c24f129276203368d20089afac8f8c
Titlinath <titlinath3@gmail.com>
2024-01-06 18:43:17 +0530

    message

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=20;
+        System.out.println("a is bigger"+(a>b));
+        System.out.println("Sum ="+ (a+b));
+    }
+}
