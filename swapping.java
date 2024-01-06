e8bcde0b111945b7bcc67581b0ffdf10baa66835
Titlinath <titlinath3@gmail.com>
2024-01-06 20:10:24 +0530

    message

---
+++ swapping.java
@@ -0,0 +1,11 @@
+public class swapping {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b= temp ;
+        System.out.println("After swapping: " + "a =" + a  + "and" + "b =" + b );
+    }
+}
