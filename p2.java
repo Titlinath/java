8806fbfdf889180640be1c4b7eb73726d25ec574
Titlinath <titlinath3@gmail.com>
2024-03-01 20:18:41 +0530

    message

---
+++ p2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class p2 {
+    public static void main(String[] args) {
+        String str1 = "java is fun";
+        for (int i = 0; i <= str1.length(); i++) {    
+        System.out.println(str1.substring(0,i));
+    }
+ }
+}
