a96c518378e7d643e769e9504a32e3267f216f01
Titlinath <titlinath3@gmail.com>
2024-04-12 22:39:58 +0530

    message

---
+++ HollowDiamond.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class HollowDiamond {
+    public static void main(String args[]) {
+        int n, i, j, m = 1;
+        System.out.print("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = n - 1;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        m = 1;
+        for (i = n - 1; i >= 1; i--) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
