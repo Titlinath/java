a88b7aeea7153c71f15a05d70827508e25956e12
Titlinath <titlinath3@gmail.com>
2024-04-25 21:27:25 +0530

    message

---
+++ Regex2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+    }
+}
+
+class MyRegex {
+   
+    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
