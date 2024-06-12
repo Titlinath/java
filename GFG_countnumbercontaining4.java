e94da1ec2a74a0451233967eb7160bb1a91cbf6b
Titlinath <titlinath3@gmail.com>
2024-06-12 17:51:25 +0530

    message

---
+++ GFG_countnumbercontaining4.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            int res = obj.countNumberswith4(n);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int countNumberswith4(int n) {
+        // code here
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (containsDigitFour(i)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    private static boolean containsDigitFour(int number) {
+        // Convert the number to string and check if it contains '4'
+        return String.valueOf(number).contains("4");
+    }
+}
