0bee39c49e59f55d79963ad15f45c621d0eaeeda
TITLI NATH <127376712+Titlinath@users.noreply.github.com>
2024-07-18 00:33:34 +0530

    Create LongestAlternatingsubsequence.java

---
+++ LongestAlternatingsubsequence.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+//Back-end complete function Template for Java
+class Solution {
+    public int alternatingMaxLength(int[] arr) {
+        // code here
+        
+        int n = arr.length;
+        if (n == 1) {
+            return 1;
+        }
+
+        int inc = 1, dec = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                inc = dec + 1;
+            } else if (arr[i] < arr[i - 1]) {
+                dec = inc + 1;
+            }
+        }
+
+        return Math.max(inc, dec);
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String s[] = br.readLine().trim().split(" ");
+            int[] nums = new int[s.length];
+            for (int i = 0; i < s.length; i++) {
+                nums[i] = Integer.parseInt(s[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.alternatingMaxLength(nums);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
